11b959e7c39d467e74083cb70aa9aec77c844f2a
BOGDANMIROSHNICHENKO <99182841+BOGDANMIROSHNICHENKO@users.noreply.github.com>
2022-11-07 20:21:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,23 @@ token='5645976423:AAHjVCXh-bsIP9RGPFkdanQ-FFx0Fe2UDaw'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id,'👋Привет,это бот - фрилансбиржа ,тут ты сможешь разместить свой заказ или заработать.Для продолжения нажми на кнопку ниже.')
+	bot.send_message(message.chat.id,reply_markup=markup,text='👋Привет,это бот - фрилансбиржа ,тут ты сможешь разместить свой заказ или заработать.Для продолжения нажми на кнопку ниже.')
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = types.KeyboardButton("Я Заказчик")
+btn2 = types.KeyboardButton("Я Исполнитель")
+markup.add(btn1, btn2)
 
+@bot.message_handler(content_types=['text'])
+def func(message):
+  if (message.text == "Я Исполнитель"):
+     btn = types.KeyboardButton("Подбор заказов")
+     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+     markup.add(btn)
+     bot.send_message(message.chat.id, reply_markup=markup,text='Выберите заказ')
 
-bot.infinity_polling()
+  elif(message.text == "Я Заказчик"):
+     btn = types.KeyboardButton("Создать заказ")
+     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+     markup.add(btn)
+     bot.send_message(message.chat.id, reply_markup=markup,text='Создайте заказ по макету анкеты:\n1.Название заказа\n2.Описание заказа\n3.Цена\n4.Фото(необязательно)\n5.Что нужно для проверки.')
+bot.polling()	
